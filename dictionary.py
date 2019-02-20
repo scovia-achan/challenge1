2f07b2df2ca10355f5297f6d7ffe8ef0c0ed3da6
Scovia <achanscovia@gmail.com>
2019-02-20 20:35:40 +0300

    modified dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,2 +1,6 @@
-numbers = {x:x**2 for x in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]}
-print(numbers)
+
+my_dictionary = {}
+for x in range(1,16):
+    x = {x:x**2}
+    my_dictionary.update(x)
+print(my_dictionary)

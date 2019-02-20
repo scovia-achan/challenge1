82aad6e463614b29767ef5238a4e75b7ef913c68
Scovia <achanscovia@gmail.com>
2019-02-20 14:30:54 +0300

    first commit

---
+++ input.py
@@ -0,0 +1,8 @@
+age = int(input("How old are you? "))
+if age < 18:
+    print("Hey, you are a minor")
+
+elif age < 36:
+    print("Hey, you are a youth")
+elif age >=36:
+    print("Hey, you are an elder")

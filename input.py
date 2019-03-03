bcb23e94be6706098937bc338a5a85a7e31297c9
Scovia <achanscovia@gmail.com>
2019-03-03 18:00:20 +0300

    age modified

--- input.py
+++ input.py
@@ -1,4 +1,13 @@
-age = int(input("How old are you? "))
+from datetime import date
+
+def calculate_age(date_of_birth):
+    today = date.today()
+    return today.year - date_of_birth.year - ((today.month, today.day) < (date_of_birth.month, date_of_birth.day))
+
+
+age = calculate_age(date(1990,12,23))
+print(age)
+
 if age < 18:
     print("Hey, you are a minor")
 
